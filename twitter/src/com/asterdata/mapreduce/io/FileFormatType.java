565936e4dc2fa91357daced36f83d8ae96d51303
ralgara <ralgara@gmail.com>
2014-04-08 22:59:13 -0400

    Add Twitter subproject (MR parser+classifier), plus POMs

--- twitter/src/com/asterdata/mapreduce/io/FileFormatType.java
+++
@@ -1,5 +0,0 @@
-package com.asterdata.mapreduce.io;
-
-public enum FileFormatType {
-	sequence,text
-}
--- twitter/src/com/asterdata/util/JobConfigDefaualts.java
+++
@@ -1,6 +0,0 @@
-package com.asterdata.util;
-
-public class JobConfigDefaualts {
-	public static final int MAX_NUMBER_OF_MAPPER_SLOTS = 84;
-	public static final int MAX_NUMBER_OF_REDUCER_SLOTS = 36;
-}
