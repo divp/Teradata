0b0fdcb7479a7e9bc0f871ac940c8414497aaa96
ralgara <ralgara@gmail.com>
2014-04-10 05:26:35 -0400

    BMS/TD: Add TD login parameters to config file

--- tpcds-benchmark/src/main/bms/benchmark/apps/tpcds/teradata/schema/run_ddl.sh
+++ tpcds-benchmark/src/main/bms/benchmark/apps/tpcds/teradata/schema/run_ddl.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+set -o nounset
+set -o errexit
+
+# Must source exports.sh in order to export global parameters defined in test properties
+. $BENCHMARK_PATH/exports.sh
+. $BENCHMARK_PATH/lib/lib.sh
+
 if [ $# -ne 1 ]
 then
     "ERROR: Expecting path to DDL script as single argument"
@@ -9,8 +16,8 @@ fi
 script_path="$1"
 
 bteq <<EOF
-.LOGON 1700v3cop1/dbc,tdc1700;
-DATABASE tpcds1000g;
+.LOGON ${BMS_TERADATA_DB_HOST}/${BMS_TERADATA_DB_UID},${BMS_TERADATA_DB_PWD};
+DATABASE ${BMS_TERADATA_DBNAME_ETL1};
 
 $(cat "$script_path")
 
