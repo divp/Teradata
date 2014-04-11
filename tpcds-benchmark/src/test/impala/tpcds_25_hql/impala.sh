ae8aa056651a1b8acd51679728b992d41d8125c3
dmoore247 <douglas.moore@thinkbiganalytics.com>
2014-04-11 05:06:11 -0700

    impala updates for the tpcds 25

--- tpcds-benchmark/src/test/impala/tpcds_25_hql/impala.sh
+++ tpcds-benchmark/src/test/impala/tpcds_25_hql/impala.sh
@@ -1,2 +1,3 @@
-# impala-shell --impalad=10.25.12.43:21000 --database=raw_tpcds1000g -f impala-sniff.sql
-impala-shell --impalad=10.25.12.43:21000 --database=raw_tpcds1000g
+#  start impala shell for interactive use
+source impala_env.sh
+impala-shell --impalad=$IMPALA_HOST:21000 --database=$TPCDS_DBNAME
--- tpcds-benchmark/src/test/impala/tpcds_25_hql/run_all.sh
+++ tpcds-benchmark/src/test/impala/tpcds_25_hql/run_all.sh
@@ -1,11 +1,31 @@
-cmd="impala-shell --impalad=10.25.12.43:21000 --database=raw_tpcds1000g"
+source impala_env.sh
+cmd="time impala-shell --impalad=$IMPALA_HOST:21000 --database=$TPCDS_DBNAME"
 $cmd -f impala-sniff.sql
-# $cmd -f q95.tpl; 
-# $cmd -f q80.tpl; 
-# $cmd -f q51.tpl; 
-# $cmd -f q10.tpl; 
-# $cmd -f q68.tpl; 
-# $cmd -f q78.tpl; 
-# $cmd -f q88.tpl; 
+
+$cmd -f q03.tpl
+$cmd -f q04.tpl
+$cmd -f q10.tpl
+# temp $cmd -f q16.tpl
+# temp $cmd -f q24.tpl
+# unsupported $cmd -f q27.tpl
+$cmd -f q29.tpl
+# temp $cmd -f q32.tpl
+$cmd -f q34.tpl
+# unsupported $cmd -f q39.tpl
+$cmd -f q42.tpl
+$cmd -f q45.tpl
+# unsupported $cmd -f q49.tpl
+$cmd -f q50.tpl
+# unsupported $cmd -f q51.tpl
+$cmd -f q62.tpl
+# temp $cmd -f q66.tpl
+$cmd -f q68.tpl
+$cmd -f q78.tpl
+# unsupported $cmd -f q80.tpl
+# temp $cmd -f q82.tpl
+$cmd -f q88.tpl
+# temp $cmd -f q92.tpl
+$cmd -f q95.tpl
+$cmd -f q97.tpl
 
 exit 0
