ab100fe6a05e6164151fa5fc9cfb9aa76e11dc52
ralgara <ralgara@gmail.com>
2014-04-12 06:20:56 -0400

    Add utility script to export fastload error data

---
+++ tpcds-benchmark/src/main/bms/benchmark/apps/tpcds/teradata/util/get_fastload_errors.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Get fastload error data
+
+set -o nounset
+set -o errexit
+
+# Must source exports.sh in order to export global parameters defined in test properties
+. $BENCHMARK_PATH/exports.sh
+. $BENCHMARK_PATH/lib/lib.sh
+
+bteq <<EOF
+    .LOGON ${BMS_TERADATA_DB_HOST}/${BMS_TERADATA_DB_UID},${BMS_TERADATA_DB_PWD};
+    .SET RECORDMODE ON
+    .EXPORT DATA FILE=/tmp/fastload_error_data.out
+    
+    LOCKING ROW FOR ACCESS
+    SELECT DataParcel
+    FROM ${BMS_TERADATA_DBNAME_ETL1}.FASTLOAD_ERR1
+    ;
+
+    .EXPORT RESET;
+    .LOGOFF;
+    .QUIT;
+EOF
