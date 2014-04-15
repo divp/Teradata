73fc5b5e2f0b182ff8ba02da3dc51a21c2bb6439
ralgara-teradata <ralgara@gmail.com>
2014-04-15 16:27:25 -0400

    TD: store dimension load - add error handling, parameterize table names

--- tpcds-benchmark/src/main/bms/benchmark/apps/tpcds/teradata/transform/load_store.sh
+++ tpcds-benchmark/src/main/bms/benchmark/apps/tpcds/teradata/transform/load_store.sh
@@ -22,31 +22,21 @@ for every row v in view V
    insert v into D including pkv as primary key and NULL as rec_end_date
    end transaction
 end-for
-
-	s_store_sk	s_store_id	s_rec_start_date	s_rec_end_date	s_closed_date_sk	s_store_name	s_number_employees	s_floor_space	s_hours	s_manager	s_market_id	s_geography_class	s_market_desc	s_market_manager	s_division_id	s_division_name	s_company_id	s_company_name	s_street_number	s_street_name	s_street_type	s_suite_number	s_city	s_county	s_state	s_zip	s_country	s_gmt_offset	s_tax_percentage
-1	1,003	AAAAAAAACAAAAAAA	2014-04-15	<NULL>	2,451,189	able                                              	245	5,250,760	8AM-4PM             	William Ward                            	2	Unknown                                                                                             	Impossible, true arms can treat constant, complete w	Charles Bartley                         	1	Unknown	1	Unknown	877	Park Laurel	Road           	Suite T   	Mount Pleasant	Gage County	NE	61933     	United States	-6.00	0.03
 EOF
 
 
-log_info "Loading store table"
-bteq_output=$(mktemp)
-bteq <<EOF > $bteq_output
+target_table='store'
+etl_view='storv'
+tmp_table=${etl_view}_tmp
+
+log_info "Loading ${target_table} table"
+bteq <<EOF 2>&1 > $log
     .LOGON ${BMS_TERADATA_DB_HOST}/${BMS_TERADATA_DB_UID},${BMS_TERADATA_DB_PWD};
     DATABASE ${BMS_TERADATA_DBNAME_ETL1};
 
-    -- Test only: clear and restore store table from backup
-    --DELETE FROM store;
-    --INSERT INTO store SELECT * FROM _bak_04150923_store;
+    DROP TABLE ${tmp_table};
     
-    SELECT 1 FROM dbc.TablesV 
-    WHERE databaseName='${BMS_TERADATA_DBNAME_ETL1}' 
-    AND tableKind='T'
-    AND tableName='storv_tmp';
-    .IF activitycount=0 THEN GOTO CREATE_TABLE
-    DROP TABLE storv_tmp;
-    .LABEL CREATE_TABLE
-    
-    CREATE TABLE storv_tmp AS (
+    CREATE TABLE ${tmp_table} AS (
     SELECT 
         s_store_sk
         ,s_store_id
@@ -77,15 +67,15 @@ bteq <<EOF > $bteq_output
         ,s_country
         ,s_gmt_offset
         ,s_tax_percentage
-    FROM storv) WITH DATA;
+    FROM ${etl_view}) WITH DATA;
     
-    UPDATE store
+    UPDATE ${target_table}
     SET 
         s_rec_end_date = CURRENT_DATE - 1
     WHERE  s_rec_end_date IS NULL
-    AND s_store_id IN (SELECT s_store_id FROM storv);
+    AND s_store_id IN (SELECT s_store_id FROM ${etl_view});
     
-    INSERT INTO store (
+    INSERT INTO ${target_table} (
         s_store_sk
         ,s_store_id
         ,s_rec_start_date
@@ -117,10 +107,20 @@ bteq <<EOF > $bteq_output
         ,s_tax_percentage
     )
     SELECT *
-    FROM storv_tmp;
+    FROM ${tmp_table} ;
 
     .LOGOFF;
     .EXIT;
 EOF
+rc=$?
+
+if [ $rc -ne 0 ]
+then
+    tail $log
+    log_error "Error loading table ${target_table}. See detail log: $log"
+    exit 1
+fi
+
+log_info "Table loaded ${target_table} successfully"
 
 
