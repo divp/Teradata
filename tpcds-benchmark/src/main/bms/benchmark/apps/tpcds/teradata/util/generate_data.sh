95243e396e348a2ce41a7d8226a7ad8af16cc324
ralgara-teradata <ralgara@gmail.com>
2014-04-16 18:03:00 -0400

    Create TPC-DS data generation script for batch updates

--- tpcds-benchmark/src/main/bms/benchmark/apps/tpcds/teradata/util/generate_data.sh
+++ tpcds-benchmark/src/main/bms/benchmark/apps/tpcds/teradata/util/generate_data.sh
@@ -2,24 +2,42 @@
 
 cd /data/tpcds/tools
 
-# Generate base 1GB set
+
 mkdir -p /data/benchmark/tpcds/sf1/00{0,1,2,3,4}
+# generate initialization set at 1GB
 dsdgen -DIR /data/benchmark/tpcds/sf1/000 -scale 1 -RNGSEED 0
+# generate 1GB update batches
 dsdgen -DIR /data/benchmark/tpcds/sf1/001 -scale 1 -RNGSEED 0 -UPDATE 1
 dsdgen -DIR /data/benchmark/tpcds/sf1/002 -scale 1 -RNGSEED 0 -UPDATE 2
 dsdgen -DIR /data/benchmark/tpcds/sf1/003 -scale 1 -RNGSEED 0 -UPDATE 3
 dsdgen -DIR /data/benchmark/tpcds/sf1/004 -scale 1 -RNGSEED 0 -UPDATE 4
 
+# soft link existing 1TB initial set
+mkdir -p /data/benchmark/tpcds/sf1000
+ln -s /data/tpcds/SF1000 /data/benchmark/tpcds/sf1000/000
 mkdir -p /data/benchmark/tpcds/sf1000/00{1,2,3,4}
-dsdgen -DIR /data/benchmark/tpcds/sf1000/000 -scale 1000 -RNGSEED 0
+# generate 1TB update batches
 dsdgen -DIR /data/benchmark/tpcds/sf1000/001 -scale 1000 -RNGSEED 0 -UPDATE 1
 dsdgen -DIR /data/benchmark/tpcds/sf1000/002 -scale 1000 -RNGSEED 0 -UPDATE 2
 dsdgen -DIR /data/benchmark/tpcds/sf1000/003 -scale 1000 -RNGSEED 0 -UPDATE 3
 dsdgen -DIR /data/benchmark/tpcds/sf1000/004 -scale 1000 -RNGSEED 0 -UPDATE 4
 
+# soft link existing 3TB initial set
+mkdir -p /data/benchmark/tpcds/sf3000
+ln -s /data/tpcds/SF3000 /data/benchmark/tpcds/sf3000/000
 mkdir -p /data/benchmark/tpcds/sf3000/00{1,2,3,4}
-dsdgen -DIR /data/benchmark/tpcds/sf3000/000 -scale 3000 -RNGSEED 0
+# generate 3TB update batches
 dsdgen -DIR /data/benchmark/tpcds/sf3000/001 -scale 3000 -RNGSEED 0 -UPDATE 1
 dsdgen -DIR /data/benchmark/tpcds/sf3000/002 -scale 3000 -RNGSEED 0 -UPDATE 2
 dsdgen -DIR /data/benchmark/tpcds/sf3000/003 -scale 3000 -RNGSEED 0 -UPDATE 3
 dsdgen -DIR /data/benchmark/tpcds/sf3000/004 -scale 3000 -RNGSEED 0 -UPDATE 4
+
+# soft link existing 10TB initial set
+mkdir -p /data/benchmark/tpcds/sf10000
+ln -s /data/tpcds/SF10000 /data/benchmark/tpcds/sf10000/000
+mkdir -p /data/benchmark/tpcds/sf10000/00{1,2,3,4}
+# generate 3TB update batches
+dsdgen -DIR /data/benchmark/tpcds/sf10000/001 -scale 10000 -RNGSEED 0 -UPDATE 1
+dsdgen -DIR /data/benchmark/tpcds/sf10000/002 -scale 10000 -RNGSEED 0 -UPDATE 2
+dsdgen -DIR /data/benchmark/tpcds/sf10000/003 -scale 10000 -RNGSEED 0 -UPDATE 3
+dsdgen -DIR /data/benchmark/tpcds/sf10000/004 -scale 10000 -RNGSEED 0 -UPDATE 4
