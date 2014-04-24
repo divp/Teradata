f850fadedb14ef3580f49459d94f84b4f0bd4f2f
ralgara-teradata <ralgara@gmail.com>
2014-04-23 22:13:30 -0400

    BMS: add passwordless SSH configuration to setup script

--- tpcds-benchmark/src/main/bms/benchmark/setup.sh
+++ tpcds-benchmark/src/main/bms/benchmark/setup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+set -o nounset
+
+. $BENCHMARK_PATH/exports.sh
+. $BENCHMARK_PATH/lib/lib.sh
+
+echo "DO NOT RUN: this script is still in early development and serves as a guide for manual installation"
+echo "Work is in progress to convert to fully automatic execution"
+exit 0
+
 if [ $(whoami) != 'root' ]
 then
     echo "Running as $(whoami). Script must run as root"
@@ -37,5 +46,46 @@ then
 fi
 chown -R $BMS_USER:users $BMS_SOURCE_DATA_PATH
 
+# Passwordless SSH
+#node=10.25.10.169
+for node in $CLUSTER_NODES
+do
+    set -o xtrace
+    echo "Configuring passwordless SSH to node $node"
+    #ssh root@${node} "useradd bms; mkdir /home/bms; chown bms:root /home/bms"
+    ssh root@${node} "chmod a+rx /data; mkdir -p /data/benchmark; chmod 755 /data/benchmark; chown bms:root /data/benchmark"
+    #echo -e "tdc\ntdc" | ssh root@${node} "passwd bms"
+    #ssh bms@${node} "[[ ! -f ~/.ssh/id_rsa.pub ]] && ssh-keygen"
+    #cat ~/.ssh/id_rsa.pub | ssh ${node} 'cat >> ~/.ssh/authorized_keys'
+    #ssh ${node} "chmod go-w ~/; chmod 700 ~/.ssh; chmod 600 ~/.ssh/authorized_keys ~/.ssh/id_rsa; chmod 644 ~/.ssh/id_rsa.pub" # ~/.ssh/known_hosts"
+    set +o xtrace 
+done
+
 sudo -u hdfs hadoop fs -mkdir /user/bms
 sudo -u hdfs hadoop fs -chown bms:hdfs /user/bms
+
+#---------------------
+
+. $BENCHMARK_PATH/exports.sh
+
+set -o nounset 
+
+$BENCHMARK_PATH/sysinfo.sh
+
+$BENCHMARK_PATH/set_security.sh
+
+[[ ! -d ${BENCHMARK_OUTPUT_PATH} ]] && mkdir -p ${BENCHMARK_OUTPUT_PATH} 2>&1
+
+sudo mkdir -p /var/opt/benchmark
+sudo chmod 666 /var/opt/benchmark
+sudo chown bms:users /var/opt/benchmark
+
+sudo mkdir /data/benchmark/temp
+sudo chmod 777 /data/benchmark/temp
+sudo chown bms:users /data/benchmark/temp
+
+# HADOOP
+sudo -u hdfs hadoop fs -mkdir /user/bms
+sudo -u hdfs hadoop fs -chown bms:hdfs /user/bms
+
+
