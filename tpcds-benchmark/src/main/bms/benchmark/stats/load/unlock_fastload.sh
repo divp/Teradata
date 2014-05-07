b5035772632eca1df5da93a9bfdad3caa1e01e37
ralgara-teradata <ralgara@gmail.com>
2014-05-07 11:38:28 -0400

    BMS: update fastload unlock script

--- tpcds-benchmark/src/main/bms/benchmark/stats/load/unlock_fastload.sh
+++ tpcds-benchmark/src/main/bms/benchmark/stats/load/unlock_fastload.sh
@@ -1,10 +1,29 @@
+#!/bin/bash
+
+set -o nounset
+
+. $BENCHMARK_PATH/exports.sh
+. $BENCHMARK_PATH/lib/lib.sh
+. $BENCHMARK_PATH/stats/load/lib.sh
+. $BENCHMARK_PATH/lib/teradata_lib.sh
+
 fastload <<EOF
 
-LOGON localhost/dbc,dbc;
+.LOGON ${BMS_STATS_DB_HOST}/${BMS_STATS_DB_UID},${BMS_STATS_DB_PWD};
 
-BEGIN LOADING benchmark.stage_sarstat ERRORFILES benchmark.ERR1, benchmark.ERR2;
-END LOADING;
-LOGOFF;
+DATABASE ${BMS_STATS_DB_NAME};
+
+DROP TABLE ERR1;
+DROP TABLE ERR2;
+
+.BEGIN LOADING stage_iostat ERRORFILES ERR1, ERR2;
+.END LOADING;
+.BEGIN LOADING stage_sarstat ERRORFILES ERR1, ERR2;
+.END LOADING;
+.BEGIN LOADING stage_vmstat ERRORFILES ERR1, ERR2;
+.END LOADING;
+.LOGOFF;
+.QUIT;
 EOF
 
 
