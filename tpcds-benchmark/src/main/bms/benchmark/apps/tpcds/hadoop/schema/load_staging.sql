97c395d4a55c60ff4bd0285358761024bd906f14
dmoore247 <douglas.moore@thinkbiganalytics.com>
2014-05-09 09:44:52 -0700

    cleanup extra load_staging.sql

--- tpcds-benchmark/src/main/bms/benchmark/apps/tpcds/hadoop/schema/load_staging.sql
+++
@@ -1,9 +0,0 @@
-use orc_tpcds1000g;
-
-INSERT OVERWRITE TABLE s_purchase
- SELECT * FROM raw_ingest_sf1000.s_purchase
-;
-
-INSERT OVERWRITE TABLE s_purchase_lineitem
- SELECT * FROM raw_ingest_sf1000.s_purchase_lineitem
-;
